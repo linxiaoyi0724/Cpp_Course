48c79107e974942b407bbf781dd778cec237d49d
xiaoyi <linyihuang@hitech.com>
2019-01-02 14:12:38 +0800

    first commit

---
+++ LearningCpp/LearningCpp/LearningCpp.cpp
@@ -0,0 +1,14 @@
+// LearningCpp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+    return 0;
+}
+
