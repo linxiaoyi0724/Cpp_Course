be4fefc2d091881ef370645b9a78de5ac805e2bf
xiaoyi <linyihuang@hitech.com>
2019-07-01 18:51:18 +0800

    linxiaoyi_0701

---
+++ LearningCpp/LearningCpp/Chapter7.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+
+
+/*
+#include <iostream>
+using namespace std;
+void sample();
+int main()
+{
+	cout << "main() will call the sample() function:" << endl;
+	sample();
+	return 0;
+}
+
+void sample()
+{
+	cout << "I'm but a simple function." << endl;
+}
+*/
+
+
+
+
---
+++ LearningCpp/LearningCpp/ExtraUsage.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+//#define _USE_MATH_DEFINES
+//#include "stdafx.h"
+//#include <math.h>
+//#include <iostream>
+//
+//using namespace std;
+//int main()
+//{
+//	char name[] = "world\n";
+//	double a = 3.13 / M_PI;
+//	std::cout << "hello " << name <<a << std::endl;
+//	return 0;
+//}
